d58e100a97a2a486f2e6ffb5649f704a9fe70e12
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-11-29 08:22:03 +0200

    Fix formatting according to PEP-8

--- helper.py
+++ helper.py
@@ -3,18 +3,21 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
+
 def set_env_for_scanning():
     get_user_config("URL", "API URL (without '/api')")
     get_user_config("TOKEN", "Token")
 
+
 def remove_trailing_slash(url):
     return url.rstrip('/')
+
 
 def get_user_config(key, title):
     os.environ[key] = input(f"\n{Fore.CYAN} {title}: {Style.RESET_ALL}").strip()
 
-def get_user_selected_product_idx(products):
 
+def get_user_selected_product_idx(products):
     if products is not None and len(products) > 0:
         val = input(f"\nSelect product (1..{len(products)}): ").strip()
 
@@ -22,21 +25,25 @@ def get_user_selected_product_idx(products):
             idx = int(val) - 1
             print_success(f"{products[idx]['title']} selected for scanning.")
             return idx
-        else :
+        else:
             print("Invalid product index")
             get_user_selected_product_idx(products)
     else:
         print("No products found!")
         exit()
 
+
 def print_info(data):
-    print(Fore.WHITE + data )
+    print(Fore.WHITE + data)
+
 
 def print_success(data):
-    print(Fore.GREEN  + data )
+    print(Fore.GREEN + data)
+
 
 def print_error(data):
-    print(Fore.RED + data )
+    print(Fore.RED + data)
+
 
 def save_file(response):
     # Extract the file_name from the Content-Disposition header using regex
@@ -51,5 +58,3 @@ def save_file(response):
         file.write(response.content)
 
     print_success(f"File saved as {file_name}")
-
-
