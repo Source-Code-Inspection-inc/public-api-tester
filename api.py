1bfba478346b4c04c15c0a37074069976f06093c
Girish Sharma <gir.sharma@gmail.com>
2023-11-29 12:02:18 +0530

    Initial commit (#1)
    1. Token authz
    2. List all the products, select one to get details
    3. Generate reports (Engineering, Executive, Executive Details)

---
+++ api.py
@@ -0,0 +1,75 @@
+import json
+import os
+import requests
+
+from helper import print_success, print_info, print_error, save_file, remove_trailing_slash
+
+# Removed config from .env to create the exe for Windows machine using pyinstaller
+VERSION = "/api/v1"
+
+
+def make_request(endpoint, method="GET", data=None):
+    headers = {
+        "Authorization": "Bearer " + os.environ['TOKEN']
+    }
+
+    url = f"{remove_trailing_slash(os.environ['URL'])}{VERSION}{endpoint}"
+
+    print_info(f"\nFetching  {url}")
+
+    if method == "GET":
+        response = requests.get(url, headers=headers)
+    elif method == "POST":
+        response = requests.post(url, headers=headers, json=data)
+    elif method == "PUT":
+        response = requests.put(url, headers=headers, json=data)
+    else:
+        raise ValueError("Invalid HTTP method")
+    return response
+
+
+def get_status():
+    endpoint = "/Status"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        if result["status"] == "OK":
+            print_success(f"Success - GET {endpoint}")
+
+
+def get_all_products():
+    endpoint = "/Products"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        products = json.loads(response.text)
+        print_success(f"Success - GET {endpoint}")
+        for idx, product in enumerate(products):
+            print_success(f"{str(idx + 1)}. {product['title']}")
+    else:
+        print_error("Access failed, check credentials or contact the administrator!")
+        exit()
+    return products
+
+
+def get_product_info(product_id):
+    endpoint = f"/Products/{product_id}"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        products = json.loads(response.text)
+        print_success(f"Success - GET {endpoint}")
+        print_success(f"Title:- {products['title']}")
+        print_success(f"Lines Of Code:- {str(products['linesOfCode'])}")
+
+
+def save_all_engineering_reports_by_scan_id(scan_id):
+    save_response_to_file(f"/reports/Engineering/{str(scan_id)}")
+    save_response_to_file(f"/reports/Executive/{str(scan_id)}")
+    save_response_to_file(f"/reports/Executive/details/{str(scan_id)}")
+    save_response_to_file(f"/reports/sbom/{str(scan_id)}")
+
+
+def save_response_to_file(endpoint):
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        print_success(f"Success - GET {endpoint}")
+        save_file(response)
---
+++ helper.py
@@ -0,0 +1,62 @@
+import os
+import re
+
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+
+def set_env_for_scanning():
+    get_user_config("URL", "API URL (without '/api')")
+    get_user_config("TOKEN", "Token")
+
+
+def remove_trailing_slash(url):
+    return url.rstrip('/')
+
+
+def get_user_config(key, title):
+    os.environ[key] = input(f"\n{Fore.CYAN} {title}: {Style.RESET_ALL}").strip()
+
+
+def get_user_selected_product_idx(products):
+    if products is not None and len(products) > 0:
+        val = input(f"\nSelect product (1..{len(products)}): ").strip()
+
+        if int(val) <= len(products):
+            idx = int(val) - 1
+            print_success(f"{products[idx]['title']} selected for scanning.")
+            return idx
+        else:
+            print("Invalid product index")
+            get_user_selected_product_idx(products)
+    else:
+        print("No products found!")
+        exit()
+
+
+def print_info(data):
+    print(Fore.WHITE + data)
+
+
+def print_success(data):
+    print(Fore.GREEN + data)
+
+
+def print_error(data):
+    print(Fore.RED + data)
+
+
+def save_file(response):
+    # Extract the file_name from the Content-Disposition header using regex
+    match = re.search(
+        r"filename=(.*?)(?:;|$)", response.headers.get("content-disposition")
+    )
+    # remove the " \ characters from the file name some file are wrapped in quotes we dont need them here
+    file_name = re.sub(r'[\\"]', '', match.group(1))
+
+    # Save the file to disk
+    with open(file_name, "wb") as file:
+        file.write(response.content)
+
+    print_success(f"File saved as {file_name}")
